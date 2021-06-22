c667cd60f3057bc9d8414e72bdb0dfab211b27d0
metal32 <ayus097836@gmail.com>
2021-06-22 20:10:16 +0530

    Added inheritance example

---
+++ InheritanceExample/InheritanceExample/Book.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace InheritanceExample
+{
+    public sealed class Book: Publication
+    {
+        public string ISBN { get; }
+        public string Author { get; }
+        public Decimal Price { get; private set; }
+        public string Currency { get; private set; }
+
+
+        public Book(string title, string author, string publisher) :
+            this(title, String.Empty, author, publisher)
+        {
+        }
+
+        public Book(string title, string isbn, string author, string publisher) : base(title, publisher, PublicationType.Book)
+        {
+            if (!String.IsNullOrEmpty(isbn))
+            {
+                if (!(isbn.Length == 10 | isbn.Length == 13))
+                    throw new ArgumentException("The ISBN must be a 10- or 13-character numeric string.");
+                ulong nISBN = 0;
+                if (!UInt64.TryParse(isbn, out nISBN))
+                    throw new ArgumentException("The ISBN can consist of numeric characters only.");
+            }
+            ISBN = isbn;
+            Author = author;
+        }
+
+        public Decimal SetPrice(Decimal price, string currency)
+        {
+            if (price < 0)
+                throw new ArgumentOutOfRangeException("The price cannot be negative.");
+            Decimal oldValue = Price;
+            Price = price;
+
+            if (currency.Length != 3)
+                throw new ArgumentException("The ISO currency symbol is a 3-character string.");
+            Currency = currency;
+
+            return oldValue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Book book = obj as Book;
+            if (book == null)
+                return false;
+            else
+                return ISBN == book.ISBN;
+        }
+
+        public override int GetHashCode()
+        {
+            return ISBN.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{(String.IsNullOrEmpty(Author) ? "" : Author + ", ")}{Title}";
+        }
+    }
+}
---
+++ InheritanceExample/InheritanceExample/Publication.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace InheritanceExample
+{
+
+    public enum PublicationType { Misc, Book, Magazine, Article };
+
+    public abstract class Publication
+    {
+
+        private int totalPages;
+        private DateTime datePublished;
+        private bool published = false;
+
+        public string Publisher { get; }
+        public string Title { get; }
+        public PublicationType Type { get; }
+        public string CopyrightName { get; private set; }
+        public int CopyrightDate { get; private set; }
+
+        public int Pages { get
+            {
+                return totalPages;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException("The number of pages can't be zero or less than zero");
+                totalPages = value;
+            }
+        }
+
+
+        public Publication(string title, string publisher, PublicationType type)
+        {
+            if (String.IsNullOrWhiteSpace(publisher))
+                throw new ArgumentException("The publisher is required");
+            this.Publisher = publisher;
+            if (String.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("The title is required");
+            this.Title = title;
+            this.Type = type;
+        }
+
+        public string GetPublicationDate()
+        {
+            if (!published)
+                return "NYP";
+            else
+                return datePublished.ToString("d");
+        }
+
+        public void Publish(DateTime datePublished)
+        {
+            this.published = true;
+            this.datePublished = datePublished;
+        }
+
+        public void Copyright(string copyrightName, int copyrightDate)
+        {
+            if (String.IsNullOrWhiteSpace(copyrightName))
+                throw new ArgumentException("The name of the copyright holder is required.");
+            this.CopyrightName = copyrightName;
+
+            int currentYear = DateTime.Now.Year;
+            if (copyrightDate < currentYear - 10 || copyrightDate > currentYear + 2)
+                throw new ArgumentException($"The copyright year must be between {currentYear - 10} and {currentYear + 1}");
+
+            this.CopyrightDate = copyrightDate;
+
+        }
+
+        public override string ToString() => Title; 
+    }
+}
